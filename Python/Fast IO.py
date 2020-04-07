bef384c278b3733f192cdad6684ef822c8726ecf
Salman Sahel <salman.exe@gmail.com>
2020-04-07 07:50:23 +0600

    Create Fast IO.py

---
+++ Python/Fast IO.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+import os
+import sys
+from io import BytesIO, IOBase
+
+def nc2(n):
+    return n * (n - 1) // 2
+
+def main():
+    t = int(input())
+    for tc in range(t):
+        n, m = map(int, input().split())
+        k = n - m
+        q = (n - m) // (m + 1)
+        r = (n - m) % (m + 1)
+        ans = nc2(n+1) - nc2(q + 1) * (m + 1 - r) - r * nc2(q + 2)
+        print(ans)
+
+# region fastio
+
+BUFSIZE = 8192
+
+
+class FastIO(IOBase):
+    newlines = 0
+
+    def __init__(self, file):
+        self._fd = file.fileno()
+        self.buffer = BytesIO()
+        self.writable = "x" in file.mode or "r" not in file.mode
+        self.write = self.buffer.write if self.writable else None
+
+    def read(self):
+        while True:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            if not b:
+                break
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines = 0
+        return self.buffer.read()
+
+    def readline(self):
+        while self.newlines == 0:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            self.newlines = b.count(b"\n") + (not b)
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines -= 1
+        return self.buffer.readline()
+
+    def flush(self):
+        if self.writable:
+            os.write(self._fd, self.buffer.getvalue())
+            self.buffer.truncate(0), self.buffer.seek(0)
+
+
+class IOWrapper(IOBase):
+    def __init__(self, file):
+        self.buffer = FastIO(file)
+        self.flush = self.buffer.flush
+        self.writable = self.buffer.writable
+        self.write = lambda s: self.buffer.write(s.encode("ascii"))
+        self.read = lambda: self.buffer.read().decode("ascii")
+        self.readline = lambda: self.buffer.readline().decode("ascii")
+
+
+sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+
+# endregion
+
+if __name__ == "__main__":
+    main()
